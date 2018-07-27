a29cfb0189767b2ca57a4dbf12e20e5479ab9f1f
Hozhiy <gun.dimonchik@gmail.com>
2018-07-27 17:36:57 +0300

    create cart-box

--- js/script.js
+++ js/script.js
@@ -571,3 +571,23 @@ $('form').submit(function(e) {
 });
 
 /* конец попап для сабмит*/
+
+/* Поле с переключателем количества */
+
+$('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
+
+/* Конец Поле с переключателем количества */
