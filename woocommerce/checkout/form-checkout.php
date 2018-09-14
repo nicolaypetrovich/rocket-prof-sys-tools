0b2820c1790b202adb767eab226e97075dda022a
mugenzo <gavreel911@gmail.com>
2018-09-14 15:03:30 +0300

    cart view update

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -54,17 +54,86 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<?php endif; ?>
 
 		<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-	</div>
 	
-	<div class="box-your-order">
-		<div class="name-container">
-			<h4 id="order_review_heading"><?php // _e( 'Your order', 'woocommerce' ); ?></h4>
-		</div>
 
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
+	<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+
 	
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	
+	</div>
+
+	<div class="box-your-order">
+
+		<div class="name-container">
+			<h4>Ваш заказ</h4>
+		</div>
+
+		<table class="shop_table woocommerce-checkout-review-order-table">
+			<thead>
+			</thead>
+			<tbody>
+			</tbody>
+			<tfoot>
+
+				<tr class="cart-subtotal">
+					<th><?php _e( 'Subtotal', 'woocommerce' ); ?></th>
+					<td><?php wc_cart_totals_subtotal_html(); ?></td>
+				</tr>
+
+				<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
+					<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+						<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
+						<td><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+					</tr>
+				<?php endforeach; ?>
+
+				<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+
+					<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
+
+					<?php wc_cart_totals_shipping_html(); ?>
+
+					<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
+
+				<?php endif; ?>
+
+				<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+					<tr class="fee">
+						<th><?php echo esc_html( $fee->name ); ?></th>
+						<td><?php wc_cart_totals_fee_html( $fee ); ?></td>
+					</tr>
+				<?php endforeach; ?>
+
+				<?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
+					<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
+						<?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : ?>
+							<tr class="tax-rate tax-rate-<?php echo sanitize_title( $code ); ?>">
+								<th><?php echo esc_html( $tax->label ); ?></th>
+								<td><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+							</tr>
+						<?php endforeach; ?>
+					<?php else : ?>
+						<tr class="tax-total">
+							<th><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
+							<td><?php wc_cart_totals_taxes_total_html(); ?></td>
+						</tr>
+					<?php endif; ?>
+				<?php endif; ?>
+
+				<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
+
+				<tr class="order-total">
+					<th><?php _e( 'Total', 'woocommerce' ); ?></th>
+					<td><?php wc_cart_totals_order_total_html(); ?></td>
+				</tr>
+
+				<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
+
+			</tfoot>
+		</table>
+
+	</div>
 
 </form>
 
